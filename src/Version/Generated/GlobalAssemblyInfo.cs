d148c1364ef8c576c72411c8d0c9445399cad02d
jglasson <jglasson.ext@autoscout24.com>
2016-01-28 18:31:04 +0100

    moved files from NCase

---
+++ src/Version/Generated/GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+	using System.Reflection;
+
+    [assembly: AssemblyProduct("NCase")]
+	[assembly: AssemblyCompany("jeromerg")]
+	[assembly: AssemblyCopyright("Apache License 2.0")]
+    [assembly:AssemblyTrademark              ("")]
+    [assembly:AssemblyCulture                ("")]
+    [assembly:AssemblyConfiguration          ("DEBUG")]
+
+	[assembly:AssemblyVersion("0.2.0.262")]
+	[assembly:AssemblyFileVersion("0.2.0.262")]
+    [assembly:AssemblyInformationalVersion   ("0.2.0.262, Hash f084f8c, BuildConfig DEBUG, HasLocalChange True")]
